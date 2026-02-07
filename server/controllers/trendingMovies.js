960febb2e4bcc76f4ad2c7729d4889ae20bb444e
Ajaz Miah <ajazmiah@yahoo.com>
2026-02-07 12:33:06 -0500

    query page to number

--- server/controllers/trendingMovies.js
+++ server/controllers/trendingMovies.js
@@ -1,9 +1,9 @@
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 export const getTrendingMovies = async (req, res) => {
-  const page = req.query.page
+  const page = parseInt(req.query.page)
 
-  const URL = `https://api.themoviedb.org/3/trending/movie/week?api_key=${TMDB_API_KEY}&page=${page}`;
+  const URL = `https://api.themoviedb.org/3/trending/movie/week?api_key=${'dcb7abed74594f9a1b16be5f0a08f440'}&page=${page}`;
   try {
     const response = await fetch(URL);
     const movies = await response.json();
