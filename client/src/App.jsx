99a22a6dbcd2dd40f609140367779764359b30ed
Ajaz Miah <ajazmiah@yahoo.com>
2026-02-07 02:58:08 -0500

    wrapped App with BrowserRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-        <Routes>
-          <Route path="/" element={<h1>hi</h1>} />
-          <Route path="/movie/:id" element={<h1>heloo</h1> }/>
-        </Routes>
-     
+       <h1>wassssuppppp</h1>
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App/>} />
+        <Route path="/movie/:id" element={<h1>heloo</h1>} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
