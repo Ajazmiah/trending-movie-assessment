461e76ed701d8b88ad45942597949d1a96c42292
Ajaz Miah <ajazmiah@yahoo.com>
2026-02-07 12:30:37 -0500

    using custom useFetch hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import useFetch from "./hooks/useFetch";
 
 function App() {
+
+
+
+
+const [page , setPage] = useState(1)
+const url = `http://localhost:5000/api/movie/trending?page=${page}`
+
+const {data, isLoading, error } = useFetch(url)
+
+
   return (
     <>
        <h1>wassssuppppp</h1>
