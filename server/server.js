cac0fa80f6cb8a9e4d681c4015fbafec734754ab
Ajaz Miah <ajazmiah@yahoo.com>
2026-02-07 13:08:24 -0500

    added allowed cros

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
-import express from 'express'
-import dotenv from 'dotenv';
-import { getTrendingMovies } from './controllers/trendingMovies.js';
-import { getMovieDetails } from './controllers/movieDetails.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import { getTrendingMovies } from "./controllers/trendingMovies.js";
+import { getMovieDetails } from "./controllers/movieDetails.js";
+import cors from 'cors'
 
 dotenv.config();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
-app.get("/api/movie/trending", getTrendingMovies)
-app.get("/api/movie/details", getMovieDetails)
+app.get("/api/movie/trending", getTrendingMovies);
+app.get("/api/movie/details", getMovieDetails);
 
-app.listen(port, () => console.log(`Server started on ${port}`))
-
+app.listen(port, () => console.log(`Server started on ${port}`));
