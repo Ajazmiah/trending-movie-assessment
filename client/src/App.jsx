db3a67dc9fd425b233c067ee7ff81422aa9f4445
Ajaz Miah <ajazmiah@yahoo.com>
2026-02-07 02:42:01 -0500

    added react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-
   return (
     <>
-      <div>
-        MOVIES
-      </div>
+        <Routes>
+          <Route path="/" element={<h1>hi</h1>} />
+          <Route path="/movie/:id" element={<h1>heloo</h1> }/>
+        </Routes>
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
