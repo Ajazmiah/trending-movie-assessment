18a2994d12ba70b4d68a318b90784aee00315acb
Ajaz Miah <ajazmiah@yahoo.com>
2026-02-07 01:29:32 -0500

    runnin server and client concurrently

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv';
+import { getTrendingMovies } from './controllers/trendingMovies.js';
 
-const port = process.env.PORT || 500
+
+dotenv.config();
+const port = process.env.PORT || 5000
 const app = express();
 
+
+app.use("/api/movie/trending", getTrendingMovies)
+
 app.listen(port, () => console.log(`Server started on ${port}`))
+
