5bae93a1cbfbcac598e3908181a4db336274fb75
Ajaz Miah <ajazmiah@yahoo.com>
2026-02-07 18:48:00 -0500

    show no fav movie when favorites is empty

--- client/src/components/Favorite/Favorite.jsx
+++ client/src/components/Favorite/Favorite.jsx
@@ -3,7 +3,7 @@ import { useMovieContext } from "../../../context/movieContext";
 import MovieCard from "../MovieCard/MovieCard";
 
 function Favorite() {
-  const { favorites, toggleFavoriteMovie, isFavorite } = useMovieContext();
+  const { favorites } = useMovieContext();
   return (
     <>
       <h1>Favorite Movies</h1>
