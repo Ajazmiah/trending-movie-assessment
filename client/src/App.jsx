fbf312adb7c546a90e91915b9a03721c95344326
Ajaz Miah <ajazmiah@yahoo.com>
2026-02-07 13:09:47 -0500

    getting the trending movies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,25 @@ import { useState } from "react";
 import useFetch from "./hooks/useFetch";
 
 function App() {
+  const [page, setPage] = useState(1);
+  const url = `http://localhost:5000/api/movie/trending?page=${page}`;
 
+  const { data, isLoading, error } = useFetch(url);
 
-
-
-const [page , setPage] = useState(1)
-const url = `http://localhost:5000/api/movie/trending?page=${page}`
-
-const {data, isLoading, error } = useFetch(url)
-
+  console.log(data)
 
   return (
     <>
-       <h1>wassssuppppp</h1>
+      <h1>Trending Movies</h1>
+      <div className="movies-grid">
+        {data?.map((movie) => (
+          <div key={movie.id} className="movie-card">
+            <img src={movie.poster_img_url} alt={movie.title} width={200} />
+            <h3>{movie.title}</h3>
+            <p>Rating: {movie.vote_average}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
