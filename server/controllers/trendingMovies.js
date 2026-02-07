8a91fed664d3aca7f6f1f94f948349155b57f09a
Ajaz Miah <ajazmiah@yahoo.com>
2026-02-07 13:09:08 -0500

    maped the results data to include poster image url

--- server/controllers/trendingMovies.js
+++ server/controllers/trendingMovies.js
@@ -2,13 +2,20 @@ const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 export const getTrendingMovies = async (req, res) => {
   const page = parseInt(req.query.page)
+  const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
   const URL = `https://api.themoviedb.org/3/trending/movie/week?api_key=${'dcb7abed74594f9a1b16be5f0a08f440'}&page=${page}`;
   try {
     const response = await fetch(URL);
-    const movies = await response.json();
+    const data = await response.json();
 
-    res.json(movies);
+    const moviesWithPosterImage = data.results.map((movie) => ({
+      ...movie,
+      poster_img_url: `${IMAGE_BASE_URL}/${movie.poster_path}`
+  
+    }));
+
+    res.json(moviesWithPosterImage);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
