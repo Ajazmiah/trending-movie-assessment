68f5cc2a260aa3db3f4fbcec78d93c09d08eb076
Ajaz Miah <ajazmiah@yahoo.com>
2026-02-07 16:12:13 -0500

    added favorite compoent to route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import Favorite from "./components/Favorite/Favorite";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App/>} />
         <Route path="/movie/:id" element={<h1>heloo</h1>} />
+        <Route path="/favorites" element={<Favorite/>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>
