2dadb681f70390ca66fdf2fad832cbc7f11d3c6e
Ajaz Miah <ajazmiah@yahoo.com>
2026-02-07 22:21:27 -0500

    styles for movie details page

--- client/src/components/MovieDetails/MovieDetails.jsx
+++ client/src/components/MovieDetails/MovieDetails.jsx
@@ -1,12 +1,58 @@
 import React from "react";
+import styles from "./MovieDetails.module.css";
 import { useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 
 function MovieDetails() {
-    const { movieId } = useParams();
+  const { movieId } = useParams();
+  const url = `http://localhost:5000/api/movie/details?movieId=${movieId}`;
+
+  const { isLoading, data: movie, error } = useFetch(url);
+  console.log(movie);
+
+  if (!movie) return;
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>error</p>;
+
   return (
-    <div>{movieId}</div>
-  )
+    <div className={styles.movieDetails}>
+      <img
+        src={movie.poster_img_url}
+        alt={movie.title}
+        className={styles.movieImg}
+      />
+
+      <div className={styles.movieInfo}>
+        <h1>{movie.title}</h1>
+
+        <p className="tagline">"{movie?.tagline}"</p>
+        <p className="overview">{movie?.overview}</p>
+        <p>
+          <strong>Genres:</strong> {movie.genres.map((g) => g.name).join(", ")}
+        </p>
+
+        <p>
+          <strong>Release:</strong> {movie.release_date}
+        </p>
+
+        <p>
+          <strong>Runtime:</strong> {movie.runtime} min
+        </p>
+
+        <p>
+          <strong>Rating:</strong> {movie.vote_average.toFixed(1)} (
+          {movie.vote_count} votes)
+        </p>
+
+        {movie.homepage && (
+          <a href={movie.homepage} target="_blank">
+            Visit Official Site
+          </a>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default MovieDetails
+export default MovieDetails;
