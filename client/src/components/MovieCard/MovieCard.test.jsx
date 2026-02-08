d2aae55f8074684cf3e1cce91dc21e2bbdd87aec
Ajaz Miah <ajazmiah@yahoo.com>
2026-02-08 01:51:31 -0500

    testing if clicking favorite change + to - to incdicate the movie already favorited

--- client/src/components/MovieCard/MovieCard.test.jsx
+++ client/src/components/MovieCard/MovieCard.test.jsx
@@ -3,6 +3,8 @@ import MovieCard from "./MovieCard";
 import { FavoriteMovieProvider } from "../../context/movieContext";
 import { MemoryRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom/vitest";
+import { expect } from "vitest";
 
 describe("MovieCard", () => {
   const mockedMovie = [
@@ -13,9 +15,7 @@ describe("MovieCard", () => {
     },
   ];
 
-  const isFavorite = false;
-  const toggleFavoriteMovie = () => {};
-  it("renders the movie title and other details", () => {
+  it("renders the movie title and ", () => {
     render(
       <FavoriteMovieProvider>
         <MemoryRouter>
@@ -24,6 +24,34 @@ describe("MovieCard", () => {
       </FavoriteMovieProvider>
     );
 
-    expect(document.body.textContent).toContain("movie title");
+    expect(screen.getByText("movie title")).toBeInTheDocument();
+  });
+
+  it("displays + when the movie is not favorited", () => {
+    render(
+      <FavoriteMovieProvider>
+        <MemoryRouter>
+          <MovieCard movies={mockedMovie} />
+        </MemoryRouter>
+      </FavoriteMovieProvider>
+    );
+
+    expect(screen.getByText("+")).toBeInTheDocument();
+  });
+
+  it("displays - when the movie is favorited", async () => {
+    const toggleFavoriteMovie = () => {};
+    render(
+      <FavoriteMovieProvider>
+        <MemoryRouter>
+          <MovieCard movies={mockedMovie} />
+        </MemoryRouter>
+      </FavoriteMovieProvider>
+    );
+
+    const btn = screen.getByTestId("favorite-btn");
+    await userEvent.click(btn);
+
+    expect(screen.getByText("-")).toBeInTheDocument();
   });
 });
