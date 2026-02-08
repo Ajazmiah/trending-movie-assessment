424f5e378249d61f0c1a39def0263b578b8f50a6
Ajaz Miah <ajazmiah@yahoo.com>
2026-02-07 22:36:53 -0500

    added optional chaining if img deosnt exist

--- server/controllers/movieDetails.js
+++ server/controllers/movieDetails.js
@@ -1,14 +1,23 @@
 export const getMovieDetails = async (req, res) => {
-  const movieId = parseInt(req.query.movieId) || 1368166;
+  const movieId = parseInt(req.query.movieId);
+
   const TMDB_API_KEY = process.env.TMDB_API_KEY;
+  const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
   const URL = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${TMDB_API_KEY}`;
+
   try {
     const response = await fetch(URL);
     const movieDetails = await response.json();
-    res.json(movieDetails);
+
+    const moviesWithPosterImage = {
+      ...movieDetails,
+      poster_img_url: `${IMAGE_BASE_URL}/${movieDetails?.belongs_to_collection?.poster_path}`,
+    };
+
+    res.json(moviesWithPosterImage);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: "Something is wrong with the server" });
   }
 };
